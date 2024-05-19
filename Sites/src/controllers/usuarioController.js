e83124e881534b6f89dc674c48351a0ece04f709
rogeriomatzak <rogerio.santiago@sptech.school>
2024-05-19 20:42:36 -0300

    Implentação Da Web-Data-Viz (Quase-Funcionado)

--- Sites/src/controllers/usuarioController.js
+++ Sites/src/controllers/usuarioController.js
@@ -56,6 +56,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var empresaId = req.body.empresaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -64,10 +65,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (empresaId == undefined) {
+        res.status(400).send("Sua empresa está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, empresaId)
             .then(
                 function (resultado) {
                     res.json(resultado);
